080f959ac48fefd81f1cc3609cae6e13f28da710
quanzee <151818329+quanzee@users.noreply.github.com>
2025-06-25 14:57:33 +1000

    Complete project

---
+++ javascript.js
@@ -0,0 +1,98 @@
+function getComputerChoice() {
+    let number = Math.random()
+
+    if (number >= 0.5) {
+        return "rock"
+    } 
+    else if (number < 0.5 && number != 0) {
+        return "paper"
+    }
+    return "scissors"
+}
+
+function getHumanChoice() {
+    let userInput = prompt("Choose rock, paper or scissors.");
+
+    return userInput
+}
+
+
+
+function capitalise(string) {
+    let firstLetter = string[0].toUpperCase();
+    let remainingLetters = string.slice(1).toLowerCase();
+
+    return firstLetter + remainingLetters
+}
+
+
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        let loseMessage = `You lose! ${capitalise(computerChoice)} beats ${humanChoice.toLowerCase()}.`;
+        let winMessage = `You win! ${capitalise(humanChoice)} beats ${computerChoice.toLowerCase()}.`;
+        let drawMessage = "It's a draw!";
+    
+        if (humanChoice.toLowerCase() === "rock") {
+            if (computerChoice === "paper") {
+                computerScore ++;
+                console.log(loseMessage);
+            }
+    
+            else if (computerChoice === "scissors") {
+                humanScore ++;
+                console.log(winMessage);
+            }
+    
+            else {
+                console.log(drawMessage);
+            }
+        }
+    
+        else if (humanChoice.toLowerCase() === "scissors") {
+            if (computerChoice === "rock") {
+                computerScore ++;
+                console.log(loseMessage);
+            }
+    
+            else if (computerChoice === "paper") {
+                humanScore ++;
+                console.log(winMessage);
+            }
+    
+            else {
+                console.log(drawMessage);
+            }
+        }
+    
+        else if (humanChoice.toLowerCase() === "paper") {
+            if (computerChoice === "scissors") {
+                computerScore ++;
+                console.log(loseMessage);
+            }
+    
+            else if (computerChoice === "rock") {
+                humanScore ++;
+                console.log(winMessage);
+            }
+    
+            else {
+                console.log(drawMessage);
+            }
+        }
+    }
+
+
+
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+
+    console.log(`Your score is ${humanScore} while the computer scored ${computerScore}.`);
+}
